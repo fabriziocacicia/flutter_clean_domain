5f9eeb6383acf0a6ae7736e71b85e0d29ba04aa8
Fabrizio Cacicia <mail@fabriziocacicia.com>
2022-04-11 16:28:35 +0200

    chore: remove the bricks from the repo (#15)

--- bricks/value_object/__brick__/{{snake_case_name}}/invalid_{{snake_case_name}}_failure.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_clean_domain/flutter_clean_domain.dart';
-
-/// Represents a failure due to an invalid {{class_name}}.
-class Invalid{{class_name}}Failure extends ValueFailure<{{{type}}}> {
-  /// Creates an [Invalid{{class_name}}Failure].
-  const Invalid{{class_name}}Failure({required {{{type}}} invalidValue})
-      : super(invalidValue: invalidValue);
-}
